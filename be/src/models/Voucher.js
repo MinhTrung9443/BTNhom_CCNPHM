65b9290cd120642a3c6fc6b925fce15f11fa86bc
MinhTrung9443 <minhtrungbttv@gmail.com>
2025-09-20 08:47:42 +0700

    them model voucher

---
+++ be/src/models/Voucher.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const voucherSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    discountValue: {
+      type: Number,
+      required: true,
+    },
+    maxDiscount: {
+      type: Number,
+      default: null, // optional, only for percentage type
+    },
+    minOrderValue: {
+      type: Number,
+      default: 0,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    usageLimit: {
+      type: Number,
+      default: null, // total times this voucher can be used
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Voucher", voucherSchema);
--- be/src/models/index.js
+++ be/src/models/index.js
@@ -6,11 +6,13 @@ import Category from "./Category.js";
 import ProductView from "./ProductView.js";
 import Cart from "./Cart.js";
 import Delivery from "./Delivery.js";
+import Voucher from "./Voucher.js";
 export {
   User,
   OTP,
   Product,
   Order,
+  Voucher,
   ProductView,
   Category,
   Cart,
