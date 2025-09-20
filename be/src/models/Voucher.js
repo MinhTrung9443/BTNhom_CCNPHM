e99b217dbe4b37276ce4c8199b674391c03834aa
MinhTrung9443 <minhtrungbttv@gmail.com>
2025-09-20 08:58:52 +0700

    update voucher

--- be/src/models/Voucher.js
+++ be/src/models/Voucher.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const voucherSchema = new mongoose.Schema(
   {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     code: {
       type: String,
       required: true,
