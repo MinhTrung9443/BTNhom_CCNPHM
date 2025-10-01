acacd3c8098d4d14f1cc3acfbedcc8e6c89def11
hnhung31 <hnhunght31.10@gmail.com>
2025-10-01 14:09:36 +0700

    Edit upload avatar

--- be/src/middlewares/upload.js
+++ be/src/middlewares/upload.js
@@ -1,10 +1,17 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 import { AppError } from '../utils/AppError.js';
+
+const uploadDir = 'uploads';
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, uploadDir); 
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
